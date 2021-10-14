32be4886f44c4b6265c4db971050d586be8ce1d3
Gustavo Tadeu <gustavo.tadeu.silva@hotmail.com>
2021-10-14 17:53:00 -0300

    Subindo exercicio modulo 16

---
+++ Modulo 16/projeto/script.js
@@ -0,0 +1,160 @@
+function taycanGo(){
+    let name = document.getElementById('name-info')
+    let text = document.getElementById('sobre-info')
+    let img = document.getElementById('img-info')
+
+    name.innerHTML = "Taycan Turbo S"
+
+    text.innerHTML = 'O espírito tem várias facetas. É mutável e está em constante movimento. Ao ver o Porsche Taycan, mostra um sorriso, expressando intenso entusiasmo. Ou no anseio de liberdade ao volante do novo Taycan Cross Turismo, que mesmo fora do cotidiano permanece no seu habitat.'
+
+    img.src = './image/taycan-info.png'
+
+        
+    let valor1 = document.getElementById("valor1")
+    let valor2 = document.getElementById("valor2")
+    let valor3 = document.getElementById("valor3")
+    let valor4 = document.getElementById("valor4")
+    let valor5 = document.getElementById("valor5")
+    let valor6 = document.getElementById("valor6")
+    let valor7 = document.getElementById("valor7")
+    let valor8 = document.getElementById("valor8")
+    let valor9 = document.getElementById("valor9")
+    let valor10 = document.getElementById("valor10")
+    
+    let dado1 = document.getElementById('dado1')
+    dado1.innerHTML = 'Capacidade bruta da bateria'
+
+    valorG1 = '<strong>-</strong>'
+    valorG2 = '<strong>500 kW/625 cv</strong>'
+    valorG3 = '<strong>260 Km/h</strong>'
+    valorG4 = '<strong>2,8 s</strong>'
+    valorG5 = '<strong>-</strong>'
+    valorG6 = '<strong>1.050 Nm</strong>'
+    valorG7 = '<strong>93,4 kWh</strong>'
+    valorG8 = '<strong>366 l</strong>'
+    valorG9 = '<strong>2.295 kg</strong>'
+    valorG10 = '<strong>4.963 mm/1.966 mm/1.378 mm</strong>'
+
+    valor1.innerHTML = valorG1 
+    valor2.innerHTML = valorG2
+    valor3.innerHTML = valorG3
+    valor4.innerHTML = valorG4
+    valor5.innerHTML = valorG5
+    valor6.innerHTML = valorG6
+    valor7.innerHTML = valorG7
+    valor8.innerHTML = valorG8
+    valor9.innerHTML = valorG9 
+    valor10.innerHTML = valorG10 
+
+
+}
+
+function cayenneGo(){
+    let name = document.getElementById('name-info')
+    let text = document.getElementById('sobre-info')
+    let img = document.getElementById('img-info')
+
+    let dado1 = document.getElementById('dado1')
+    dado1.innerHTML = 'Tanque de combustível'
+    
+    
+
+
+    name.innerHTML = "Cayenne Turbo"
+
+    text.innerHTML = 'O Cayenne e o Cayenne Coupé: ambos cativam pela aparência marcante e desempenho convincente - com até 5 bancos. Se você prefere ir de Cayenne com os amigos à próxima aventura ou se você tem uma queda pelas linhas icônicas Coupé, fica por conta do seu gosto pessoal. Mas uma coisa também se aplica naturalmente a nossos dois maiores modelos: todo Porsche é primeiramente um carro esportivo.'
+
+    img.src = './image/cayenne-info.jpg'
+
+            
+    let valor1 = document.getElementById("valor1")
+    let valor2 = document.getElementById("valor2")
+    let valor3 = document.getElementById("valor3")
+    let valor4 = document.getElementById("valor4")
+    let valor5 = document.getElementById("valor5")
+    let valor6 = document.getElementById("valor6")
+    let valor7 = document.getElementById("valor7")
+    let valor8 = document.getElementById("valor8")
+    let valor9 = document.getElementById("valor9")
+    let valor10 = document.getElementById("valor10")
+    
+    
+
+    valorG1 = '<strong>8</strong>'
+    valorG2 = '<strong>404 kW/550 cv</strong>'
+    valorG3 = '<strong>286 Km/h</strong>'
+    valorG4 = '<strong>4,1 s</strong>'
+    valorG5 = '<strong>6.500 r/min</strong>'
+    valorG6 = '<strong>770 Nm</strong>'
+    valorG7 = '<strong>90 l</strong>'
+    valorG8 = '<strong>745 l</strong>'
+    valorG9 = '<strong>2.295 kg</strong>'
+    valorG10 = '<strong>4.926 mm/1.983 mm/1.673 mm</strong>'
+
+    valor1.innerHTML = valorG1 
+    valor2.innerHTML = valorG2
+    valor3.innerHTML = valorG3
+    valor4.innerHTML = valorG4
+    valor5.innerHTML = valorG5
+    valor6.innerHTML = valorG6
+    valor7.innerHTML = valorG7
+    valor8.innerHTML = valorG8
+    valor9.innerHTML = valorG9 
+    valor10.innerHTML = valorG10 
+
+} 
+
+
+function panameraGo(){
+    let name = document.getElementById('name-info')
+    let text = document.getElementById('sobre-info')
+    let img = document.getElementById('img-info')
+
+
+    let dado1 = document.getElementById('dado1')
+    dado1.innerHTML = 'Tanque de combustível'
+
+    name.innerHTML = "Panamera Turbo S E-Hybrid  "
+
+    text.innerHTML = '  Uma ideia precisa ser lógica? E sobretudo, ela deveria ser? Melhor não. Acreditamos que quanto mais visionária uma ideia é no começo, mais emocionante ela é ao final. Aí sim vale a pena lutar por ela. Foi assim também com o Panamera. Um carro esportivo para 4 pessoas? Com um desempenho surpreendente e conforto evelado?<br><br>    Com a dinâmica típica de um Porsche e, ao mesmo tempo, altamente eficiente? Impossível, diziam muitos. Audacioso, diziam outros.'
+    
+
+    img.src = './image/panamera-info.jpg'
+
+                
+    let valor1 = document.getElementById("valor1")
+    let valor2 = document.getElementById("valor2")
+    let valor3 = document.getElementById("valor3")
+    let valor4 = document.getElementById("valor4")
+    let valor5 = document.getElementById("valor5")
+    let valor6 = document.getElementById("valor6")
+    let valor7 = document.getElementById("valor7")
+    let valor8 = document.getElementById("valor8")
+    let valor9 = document.getElementById("valor9")
+    let valor10 = document.getElementById("valor10")
+    
+    
+
+    valorG1 = '<strong>8</strong>'
+    valorG2 = '<strong>514 kW/700 cv</strong>'
+    valorG3 = '<strong>315 Km/h</strong>'
+    valorG4 = '<strong>3,2 s</strong>'
+    valorG5 = '<strong>5.750 - 6.000 r/min</strong>'
+    valorG6 = '<strong>770 Nm</strong>'
+    valorG7 = '<strong>80 l</strong>'
+    valorG8 = '<strong>403 l</strong>'
+    valorG9 = '<strong>2.350 kg</strong>'
+    valorG10 = '<strong>5.049 mm/1.937 mm/1.427 mm</strong>'
+
+    valor1.innerHTML = valorG1 
+    valor2.innerHTML = valorG2
+    valor3.innerHTML = valorG3
+    valor4.innerHTML = valorG4
+    valor5.innerHTML = valorG5
+    valor6.innerHTML = valorG6
+    valor7.innerHTML = valorG7
+    valor8.innerHTML = valorG8
+    valor9.innerHTML = valorG9 
+    valor10.innerHTML = valorG10 
+
+}
